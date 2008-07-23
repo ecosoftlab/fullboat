39d9a9c518e2911d563e7b4c6d4be78ff6b99cfc
Mattt Thompson <mail@matttthompson.com>
2008-07-23 03:02:13 -0700

    Fixed Last.fm webservice

--- app/models/webservices/last_fm_observer.rb
+++ app/models/webservices/last_fm_observer.rb
@@ -1,4 +1,5 @@
 class LastFmObserver < ActiveRecord::Observer
+  require 'open-uri'
   require 'scrobbler'
     
   observe :album, :artist
@@ -17,15 +18,22 @@ protected
   def lookup_album(album)
     last_fm_album     = Scrobbler::Album.new(album.artist.name, album.name, :include_info => true)
     #album.image = last_fm_album.image_large
-    album.mbid        = last_fm_album.mbid
+    # album.mbid        = last_fm_album.mbid
     album.tracks      = last_fm_album.tracks
     album.released_on = last_fm_album.release_date
+    
+    if url = (last_fm_album.image_large || last_fm_album.image_medium || last_fm_album.image_small)
+      tempfile = Tempfile.new("#{Time.now.to_i}.jpg", File.join(RAILS_ROOT, 'tmp'))
+      tempfile.puts open(url).read
+      album.cover = tempfile
+    end
+    
     album.save
   end
   
   def lookup_artist(artist)
     last_fm_artist = Scrobbler::Artist.new(artist.name)
-    artist.mbid    = last_fm_artist.mdid
+    # artist.mbid    = last_fm_artist.mdid
     artist.save
   end
 end
