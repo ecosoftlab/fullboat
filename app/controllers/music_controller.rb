bc9dfa59dc379c56ced5489e5d90c219e3150940
Mattt Thompson <mail@matttthompson.com>
2009-01-05 21:44:58 -0500

    Bulk fixes to section pages

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -2,11 +2,7 @@ class MusicController < ApplicationController
   layout 'music'
   
   def index
-    @plays   = Play.find(:all, 
-                         :conditions => ["playable_type = ? AND created_at BETWEEN ? AND ?", 
-                                        "Album", Time.now.beginning_of_week, Time.now],
-                         :order => "created_at DESC")
-    @albums  = @plays.collect{|p| p.playable}.sort_by{|p| @plays.collect{|p| p.playable}.grep(p).length}
-    @reviews = Review.find(:all, :order => "created_at DESC", :limit => 5)
+    @recent_albums = Album.recent(:limit => 6)
+    @top_albums  = Album.most_played
   end
 end
